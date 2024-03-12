4dda61036074aaf3d59e79e0f8fe84928c1fff52
Lucas <66272629+MixDevCode@users.noreply.github.com>
2024-03-12 03:10:57 -0300

    changed img and years

--- assets/js/terminal.js
+++ assets/js/terminal.js
@@ -11,7 +11,7 @@ typed = new Typed("#terminal__prompt--text", {
     self.cursor.remove();
     typed2 = new Typed("#terminal__prompt--text-2", {
       strings: [
-        '\n<h1>Hello! My name is Lucas.</h1>^1000\n<h3>I am a Software Developer, currently graduated as a <b>Superior Technician in Software Development</b>.</h3>^1000\n<h3>I am 21 years old and <b>passionate</b> about programming (evidenced by my <a href="https://github.com/MixDevCode/" class="text-decoration-none" target="_blank">GitHub</a>).</h3>^1000<h3>I have worked on projects using languages like NodeJS, PHP, C#, and Python; you can check out some of my projects by clicking <a href="#" onClick="moveToProjects();return false;" class="text-decoration-none">here</a>.</h3>^1000',
+        '\n<h1>Hello! My name is Lucas.</h1>^1000\n<h3>I am a Software Developer, currently graduated as a <b>Superior Technician in Software Development</b>.</h3>^1000\n<h3>I am 22 years old and <b>passionate</b> about programming (evidenced by my <a href="https://github.com/MixDevCode/" class="text-decoration-none" target="_blank">GitHub</a>).</h3>^1000<h3>I have worked on projects using languages like NodeJS, PHP, C#, and Python; you can check out some of my projects by clicking <a href="#" onClick="moveToProjects();return false;" class="text-decoration-none">here</a>.</h3>^1000',
       ],
       typeSpeed: 0,
       loop: false,
